b96ca307deec8537ae6c0d7e7c08a8fe10a86058
kolmus <lukasz.kolmus@gmail.com>
2022-01-12 16:28:34 +0100

    add quantity and foundations dynamic counters

--- charity_donation/charity_app/views.py
+++ charity_donation/charity_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.views import View
 
+from .models import Donation
+
 class LandingPage(View):
     def get(self, request):
-        return render(request, "charity_app/index.html")
+        # donations = Donation.objects.fiter(user=request.user)
+        donations = Donation.objects.all()
+        foundations = []
+        quantity = 0
+        
+        for donation in donations:
+            if donation.institution not in foundations:
+                foundations.append(donation)
+            quantity += int(donation.quantity)
+        
+        answer = {
+            'foundations': len(foundations),
+            'quantity': quantity
+        }
+        return render(request, "charity_app/index.html", answer)
 
 
 class AddDonation(View):
